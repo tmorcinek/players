47b751aff26a8b160cde7d7409c5de84a9c75b0e
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2019-11-11 20:09:44 +0100

    Work in progress on Funino Tournament.

--- app/src/main/java/com/morcinek/players/TeamsGenerator.kt
+++ app/src/main/java/com/morcinek/players/TeamsGenerator.kt
@@ -1,5 +1,9 @@
 package com.morcinek.players
 
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.roundToInt
+
 
 private const val numberOfPlayersInTheTeam = 3
 
@@ -12,29 +16,32 @@ class TeamsGenerator {
         it to list
     }
 
-    fun opponents(players: List<Int>, teamBalance: Pair<Int, List<Int>>) : List<Pair<Int,Int>> =
-        players.mapNotNull { player ->  if(player == teamBalance.first) null else player to teamBalance.second.count { it == player } }.sortedBy { it.second }
+    fun opponents(players: List<Int>, teamBalance: Pair<Int, List<Int>>): List<Pair<Int, Int>> =
+        players.mapNotNull { player -> if (player == teamBalance.first) null else player to teamBalance.second.count { it == player } }.sortedBy { it.second }
 
-    private fun balanceScore(balance : List<Pair<Int, List<Int>>>) = balance.map { it.second.size }.sortedDescending().let { it.last() - it.first() }
+    private fun balanceScore(balance: List<Pair<Int, List<Int>>>) = balance.map { it.second.size }.sortedDescending().let { it.first() - it.last() } / 2
 
-    private fun opponentsScore(opponents: List<Pair<Int, List<Pair<Int, Int>>>>) = opponents.sumBy { it.second.let { list -> list.last().second - list.first().second } }
-
-    fun score(opponents: List<Pair<Int,Int>>) = opponents.last().second - opponents.first().second
+    //    private fun opponentsScore(opponents: List<Pair<Int, List<Pair<Int, Int>>>>): Int = opponents.sumBy { it.second.let { list -> 2.0.pow(list.last().second - list.first().second) }.toInt() }
+//    private fun opponentsScore(opponents: List<Pair<Int, List<Pair<Int, Int>>>>): Int = opponents.sumBy { it.second.map { it.second }.sorted().let { 2.0.pow(- it.first() + it.last() ).toInt() } }
+    //TODO median algorithm below is better in assuming the correct result
+    private fun opponentsScore(opponents: List<Pair<Int, List<Pair<Int, Int>>>>): Int = opponents.sumBy {
+        it.second.map { it.second }.let { games ->
+            games.sumBy {
+                2.0.pow(abs(games.average().roundToInt() - it)).toInt() - 1
+            }
+        }
+    }
 
     @Deprecated("use opponentsData")
     fun playersOpponents(players: List<Int>, data: List<Set<Int>>) = balanceN(players, data).map { it.first to opponents(players, it) }
 
-    fun opponentsData(balance: List<Pair<Int, List<Int>>>): List<Pair<Int, List<Pair<Int, Int>>>> = balance.map { it.first to opponents(balance.map(Pair<Int, List<Int>>::first), it) }
+    fun opponentsData(balance: List<Pair<Int, List<Int>>>): List<Pair<Int, List<Pair<Int, Int>>>> =
+        balance.map { it.first to opponents(balance.map(Pair<Int, List<Int>>::first), it) }
 
-    fun dataScore(balance: List<Pair<Int, List<Int>>>) = balanceScore(balance) * balance.size + opponentsScore(opponentsData(balance))
-//    fun nextTeam(players: List<Int>, data: List<Set<Int>>): Set<Int>{
-//        val playersOpponents = playersOpponents(players, data)
-//        val firstPlayer = playersOpponents.first()
-//        return mutableSetOf<Int>().apply {
-//            add(firstPlayer.first)
-//            addAll(firstPlayer.second.take(numberOfPlayersInTheTeam- 1).map { it.first })
-//        }
-//    }
+    fun dataScore(balance: List<Pair<Int, List<Int>>>): Pair<Int, Int> = balanceScore(balance) to opponentsScore(opponentsData(balance))
+
+    fun take(balance: List<Pair<Int, List<Int>>>, number: Int): List<Int> = balance.shuffled().sortedBy { it.second.size }.take(number).map { it.first }
+//    fun take(balance: List<Pair<Int, List<Int>>>, number: Int): List<Int> = balance.shuffled().take(number).map { it.first }
 }
 
 fun <T> List<Pair<Set<T>, Set<T>>>.flatten() = flatMap { listOf(it.first, it.second) }
--- app/src/test/java/com/morcinek/players/TeamsGeneratorTest.kt
+++ app/src/test/java/com/morcinek/players/TeamsGeneratorTest.kt
@@ -96,7 +96,7 @@ class TeamsGeneratorTest {
     @Test
     fun dataScore_step2() {
         val players = (1..6).toList()
-        val pairs = listOf(setOf(1, 2, 3) to setOf(4, 5, 6), setOf(1, 4, 5) to setOf(2, 3, 6))
+        val pairs = listOf(setOf(1, 2, 3) to setOf(4, 5, 6), setOf(1, 4, 5) to setOf(2, 3, 6), setOf(1, 6, 5) to setOf(3, 4, 2), setOf(1, 3, 5) to setOf(2, 4, 6))
 //        val pairs = listOf(setOf(1, 2, 3) to setOf(4, 5, 6))
 
         teamsGenerator.opponentsData(teamsGenerator.balance(players, pairs)).forEachIndexed { index, it ->
@@ -105,26 +105,52 @@ class TeamsGeneratorTest {
         println(teamsGenerator.dataScore(teamsGenerator.balance(players, pairs)))
     }
 
-//    @Test
-//    fun nextTeam_initial() {
-//        val players = (1..6).toList()
-//
-//        val nextTeam = teamsGenerator.nextTeam(players, listOf())
-//        assertEquals(setOf(1, 2, 3), nextTeam)
-//    }
-//
-//    @Test
-//    fun nextTeam_step1() {
-//        val players = (1..6).toList()
-////        val pairs = listOf(setOf(1, 2, 3) to setOf(4, 5, 6), setOf(1, 4, 5) to setOf(2, 3, 6))
-//        val pairs = listOf(setOf(1, 2, 3) to setOf(4, 5, 6))
-//
-//        val nextTeam = teamsGenerator.nextTeam(players, pairs.flatten())
-//        println(nextTeam)
-////        teamsGenerator.playersOpponents(players, pairs.flatten()).forEachIndexed { index, it ->
-////            println("${it.first}: ${it.second}")
-////        }
-//    }
+    @Test
+    fun dataScore_take3() {
+        val players = (0..19).toList()
+        val pairs = mutableListOf<Pair<Set<Int>, Set<Int>>>()
+        (1..36).forEach { index ->
+            val balance = teamsGenerator.balance(players, pairs)
+            var bestPairScore: Pair<Int, Pair<Set<Int>, Set<Int>>>? = null
+            repeat(6000) {
+                val updatedList = teamsGenerator.take(balance, 6).shuffled()
+                //                println(updatedList)
+                val game = updatedList.take(3).toSet() to updatedList.subList(3, 6).toSet()
+                val score = teamsGenerator.dataScore(teamsGenerator.balance(players, pairs.plus(game))).second
+                if (bestPairScore == null || score < bestPairScore!!.first) {
+                    bestPairScore = score to game
+                }
+            }
+            println("Index: $index ,Score: ${bestPairScore!!.first}")
+            pairs.add(bestPairScore!!.second)
+        }
+//        println(pairs)
+        print("val pairs${players.size} = listOf(")
+        pairs.forEachIndexed { index, it ->
+            print("setOf(${it.first.joinToString(",")}) to setOf(${it.second.joinToString(",")})")
+            if (index < pairs.size - 1) print(",")
+        }
+        println(")")
+        val balance = teamsGenerator.balance(players, pairs)
+        teamsGenerator.opponentsData(balance).forEach {
+            println("${it.first}: ${it.second}")
+        }
+        println(teamsGenerator.dataScore(balance))
+        //todo znajdz sobie 2000 kombinacji i z nich wybieraj
+    }
+
+    @Test
+    fun playersTest() {
+        val players = (0..7).toList()
+        val pairs9 = listOf(setOf(5,4,3) to setOf(6,2,7),setOf(8,6,3) to setOf(0,1,7),setOf(1,2,4) to setOf(8,5,0),setOf(4,8,7) to setOf(0,2,3),setOf(0,4,6) to setOf(5,7,1),setOf(6,3,1) to setOf(8,5,2),setOf(6,5,2) to setOf(8,7,3),setOf(4,8,1) to setOf(7,5,0),setOf(3,2,1) to setOf(4,0,6),setOf(4,3,5) to setOf(8,1,6),setOf(7,4,2) to setOf(1,3,0),setOf(8,2,0) to setOf(7,5,6),setOf(8,7,3) to setOf(5,1,4),setOf(0,3,4) to setOf(2,6,8),setOf(2,0,7) to setOf(5,6,1),setOf(2,5,3) to setOf(4,6,7),setOf(0,5,8) to setOf(2,4,1),setOf(3,0,6) to setOf(1,7,8),setOf(6,0,1) to setOf(4,8,5),setOf(0,1,8) to setOf(2,3,7),setOf(3,4,7) to setOf(5,2,6),setOf(6,8,3) to setOf(7,0,5),setOf(2,4,8) to setOf(3,1,5),setOf(0,4,6) to setOf(7,1,2),setOf(7,3,6) to setOf(2,4,0),setOf(0,5,8) to setOf(3,1,4),setOf(6,5,2) to setOf(1,8,7),setOf(4,5,7) to setOf(3,0,2),setOf(1,0,5) to setOf(8,6,4),setOf(7,1,6) to setOf(3,2,8),setOf(0,2,1) to setOf(3,7,5),setOf(7,0,8) to setOf(4,6,1),setOf(6,3,5) to setOf(4,8,2),setOf(7,2,6) to setOf(3,8,1),setOf(0,3,4) to setOf(2,1,5),setOf(5,8,4) to setOf(6,0,7))
+
+        val balance = teamsGenerator.balance(players, pairs9.take(12))// 20, 30
+        balance.println()
+        teamsGenerator.opponentsData(balance).forEach {
+            println("${it.first}: ${it.second}")
+        }
+        println(teamsGenerator.dataScore(balance))
+    }
 }
 
 private fun <T, B> List<Pair<T, B>>.println() {
