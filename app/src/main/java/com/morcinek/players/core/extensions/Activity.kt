b6bf5cca16bb0a55a52ecdbba4ac2fd4ba98a6bd
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2019-11-21 19:23:58 +0100

    Working signin flow.

---
+++ app/src/main/java/com/morcinek/players/core/extensions/Activity.kt
@@ -0,0 +1,12 @@
+package com.morcinek.players.core.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+
+
+inline fun <reified T : Activity> Context.createIntent(function: Intent.() -> Unit = {}) = Intent(this, T::class.java).apply(function)
+
+inline fun <reified T : Activity> Context.startActivity(function: Intent.() -> Unit = {}) = startActivity(createIntent<T>(function))
+
+fun Activity.startActivityForResult(intent: Intent, requestCode: Int = 0) = startActivityForResult(intent, requestCode)
--- app/src/main/java/com/morcinek/players/ui/SplashActivity.kt
+++ app/src/main/java/com/morcinek/players/ui/SplashActivity.kt
@@ -5,16 +5,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.morcinek.players.R
+import com.morcinek.players.core.extensions.startActivity
+import com.morcinek.players.core.extensions.startActivityForResult
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        FirebaseAuth.getInstance().currentUser?.let { } ?: startSignInActivity()
+        when (FirebaseAuth.getInstance().currentUser) {
+            null -> startSignInActivity()
+            else -> finishAndStartNextActivity()
+        }
     }
 
     private fun startSignInActivity() = startActivityForResult(
@@ -28,19 +32,18 @@ class SplashActivity : AppCompatActivity() {
                 )
             )
             .setTheme(R.style.AppTheme_NoActionBar_Splash)
-            .build(),
-        0
+            .build()
     )
+
+    private fun finishAndStartNextActivity() {
+        startActivity<NavActivity>()
+        finish()
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        val response = IdpResponse.fromResultIntent(data)
-
         if (resultCode == Activity.RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            // ...
+            finishAndStartNextActivity()
         } else {
             finish()
         }
