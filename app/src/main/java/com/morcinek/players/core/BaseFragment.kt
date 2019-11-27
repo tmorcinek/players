98c631dd6ef6a8f922ad8636ba0b348052e856e2
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2019-11-27 14:06:28 +0100

    Menu for fragments working.

--- app/src/main/java/com/morcinek/players/core/BaseFragment.kt
+++ app/src/main/java/com/morcinek/players/core/BaseFragment.kt
@@ -1,9 +1,7 @@
 package com.morcinek.players.core
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.morcinek.players.R
@@ -14,6 +12,7 @@ abstract class BaseFragment : Fragment() {
 
     protected abstract val layoutResourceId: Int
 
+    open val menuConfiguration: MenuConfiguration? = null
     open val fabConfiguration: FabConfiguration? = null
 
     final override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
@@ -40,6 +39,27 @@ abstract class BaseFragment : Fragment() {
             }
         }
     }
+
+    final override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        menuConfiguration?.let { setHasOptionsMenu(true) }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        menuConfiguration?.let {
+            it.actions[item.itemId]?.let { action ->
+                action.invoke()
+                return true
+            }
+        } ?: return false
+    }
+
+    final override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menuConfiguration?.let { inflater.inflate(it.menuResourceId, menu) }
+    }
 }
 
 class FabConfiguration(val fabActon: (View) -> Unit, val fabIcon: Int = R.drawable.ic_add)
+class MenuConfiguration(val menuResourceId: Int, vararg actionArgs: Pair<Int, () -> Unit>) {
+    val actions: Map<Int, () -> Unit> = actionArgs.toMap()
+}
--- app/src/main/java/com/morcinek/players/ui/players/details/PlayerDetailsFragment.kt
+++ app/src/main/java/com/morcinek/players/ui/players/details/PlayerDetailsFragment.kt
@@ -2,11 +2,13 @@ package com.morcinek.players.ui.players.details
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
 import com.morcinek.players.R
 import com.morcinek.players.core.BaseFragment
+import com.morcinek.players.core.MenuConfiguration
 import com.morcinek.players.core.data.PlayerData
 import com.morcinek.players.core.database.FirebaseReferences
 import com.morcinek.players.core.database.map
@@ -15,16 +17,22 @@ import com.morcinek.players.core.database.teamsLiveDataForValueListener
 import com.morcinek.players.core.extensions.getParcelable
 import com.morcinek.players.core.extensions.toStandardString
 import com.morcinek.players.core.extensions.viewModelWithFragment
+import com.morcinek.players.ui.lazyNavController
 import kotlinx.android.synthetic.main.fragment_player.view.*
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.dsl.module
 
 class PlayerDetailsFragment : BaseFragment() {
 
     override val layoutResourceId = R.layout.fragment_player
+    override val menuConfiguration = MenuConfiguration(R.menu.player_details,
+        R.id.delete to { Toast.makeText(requireContext(), "Delete", Toast.LENGTH_SHORT).show() },
+        R.id.edit to { Toast.makeText(requireContext(), "Edit", Toast.LENGTH_SHORT).show() }
+    )
 
     private val viewModel by viewModelWithFragment<PlayerDetailsViewModel>()
+
+    private val navController: NavController by lazyNavController()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,8 +40,9 @@ class PlayerDetailsFragment : BaseFragment() {
             view.name.text = playerData.toString()
             view.birthDate.text = playerData.getBirthDate().toStandardString()
             playerTeam.observe(this@PlayerDetailsFragment) {
-                view.team.text = it?.name ?: "NO TEAM"
+                view.team.text = it?.name ?: "-"
             }
+//            navController.
         }
     }
 }
